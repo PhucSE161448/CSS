565c022c72763bc97cc1239790410eed52a5bb18
Phúcc <112921620+PhucSE161448@users.noreply.github.com>
2024-10-19 22:39:52 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Mẹ Yêu';
+const giftUrl = 'https://www.youtube.com/shorts/ezJkLmMEzUM';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Mẹ';
+const contentCard = 'Chúc mẹ iu của con 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với mẹ trong hôm nay và cả những ngày sau';
+const giftImage = '1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
